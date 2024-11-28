202bed6e5b7a3dce3576f55c10df587d172668ff
takejohn <takejohn@takejohn.jp>
2024-11-28 20:53:19 +0900

    tmp

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs';
+import * as fs from 'fs/promises';
 import * as readline from 'readline';
 import chalk from 'chalk';
-import { Parser, Interpreter, errors, utils } from '@syuilo/aiscript';
+import { Parser, Interpreter, errors, utils, values } from '@syuilo/aiscript';
 const { AiScriptError } = errors;
 const { valToString } = utils;
 
@@ -30,7 +30,15 @@ if (filename == null) {
 	process.exit(1);
 }
 
-const script = fs.readFileSync(filename, 'utf8');
+const script = await fs.readFile(filename, 'utf8');
+for (const line of script.split(/\n|\r\n?/g)) {
+	const match = /\/\/\/\s*include*"(.*)"\s*/.exec(line);
+	if (match == null) {
+		break;
+	}
+	const filename = match[0];
+}
+
 try {
 	const ast = Parser.parse(script);
 	await interpreter.exec(ast);
