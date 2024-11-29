b10247f6dddb05d5e17a1320b663afef4b6d63fd
takejohn <takejohn@takejohn.jp>
2024-11-29 15:57:14 +0900

    テスト関数

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,9 +1,8 @@
 import * as fs from 'fs/promises';
 import chalk from 'chalk';
-import { Parser, Interpreter, errors, utils, values } from '@syuilo/aiscript';
+import { Parser, Interpreter, errors, Ast } from '@syuilo/aiscript';
 import path from 'path';
 import { AiScriptError } from '@syuilo/aiscript/error.js';
-const { valToString } = utils;
 
 const TEST_DIRNAME = 'tests';
 
@@ -17,7 +16,7 @@ interface TestSuccess extends TestResultBase {
 
 interface TestError {
 	message: string;
-	cause: AiScriptError;
+	cause?: AiScriptError;
 }
 
 interface TestFailure extends TestResultBase {
@@ -27,74 +26,126 @@ interface TestFailure extends TestResultBase {
 
 type TestResult = TestSuccess | TestFailure;
 
+class ErrorHandler {
+	public readonly errors: TestError[] = [];
+
+	public addError(error: TestError): void {
+		this.errors.push(error);
+	}
+
+	public catch(message: string, e: unknown): void {
+		if (e instanceof AiScriptError) {
+			this.addError({
+				message,
+				cause: e,
+			});
+		} else {
+			throw e;
+		}
+	}
+
+	public toResult(): TestResult {
+		if (this.errors.length == 0) {
+			return { success: true };
+		} else {
+			return { success: false, errors: this.errors };
+		}
+	}
+}
+
+function parse(script: string, errorHandler: ErrorHandler): Ast.Node[] | undefined {
+	try {
+		return Parser.parse(script);
+	} catch (e) {
+		errorHandler.catch('parser', e);
+	}
+}
+
 async function runTest(filename: string): Promise<TestResult> {
-	const failureErrors: TestError[] = [];
+	const errorHandler = new ErrorHandler();
+	let currentExecuting = 'root';
 
 	const interpreter = new Interpreter({}, {
 		in() {
 			return Promise.reject(new Error('Cannot use standard input during test'));
 		},
-		out(value) {
-			console.log(chalk.magenta(valToString(value, true)));
-		},
 		err(e) {
-			failureErrors.push({
-				message: 'Error occurred while running',
-				cause: e,
-			});
+			errorHandler.catch(currentExecuting, e);
 		},
 	});
 
 	const dirname = path.dirname(filename);
 	const script = await fs.readFile(filename, 'utf8');
+	const ast = parse(script, errorHandler);
+	if (ast == null) {
+		return errorHandler.toResult();
+	}
 
-	try {
+	const imports: unknown = Interpreter.collectMetadata(ast)?.get('imports');
+	if (!(imports instanceof Array && imports.every((filename) => typeof filename == 'string'))) {
+		throw new errors.AiScriptTypeError('Unexpected type of imports');
+	}
+
+	const importScripts = await Promise.all(imports.map(filename => fs.readFile(path.resolve(dirname, filename), 'utf8')));
+	for (const script of importScripts) {
 		const ast = Parser.parse(script);
-		const imports: unknown = Interpreter.collectMetadata(ast)?.get('imports');
-		if (imports instanceof Array && imports.every((filename) => typeof filename == 'string')) {
-			const scripts = await Promise.all(imports.map(filename => fs.readFile(path.resolve(dirname, filename), 'utf8')));
-			for (const script of scripts) {
-				const ast = Parser.parse(script);
-				await interpreter.exec(ast);
-			}
-		} else {
-			throw new errors.AiScriptTypeError('Unexpected type of imports');
-		}
-		await interpreter.exec(ast);
-		if (failureErrors.length == 0) {
-			return { success: true };
-		} else {
-			return { success: false, errors: failureErrors };
-		}
-	} catch (e) {
-		if (e instanceof AiScriptError) {
-			return {
-				success: false,
-				errors: [{
-					message: 'Error occurred while running',
-					cause: e,
-				}],
-			};
-		} else {
-			throw e;
+		try {
+			await interpreter.exec(ast);
+		} catch (e) {
+			errorHandler.catch(`import ${filename}`, e);
 		}
 	}
+
+	try {
+		await interpreter.exec(ast);
+	} catch (e) {
+		errorHandler.catch('root', e);
+	}
+
+	for (const [name, { value }] of interpreter.scope.getAll().entries()) {
+		if (value.attr == null) {
+			continue;
+		}
+		for (const attr of value.attr) {
+			if (attr.name != 'test') {
+				errorHandler.addError({ message: `Unknown attribute for variable \`${name}\`: \`${attr.name}\`` });
+				continue;
+			}
+			if (value.type != 'fn') {
+				errorHandler.addError({ message: `\`${name}\` is ${value.type}, but has 'test' attribute`});
+				continue;
+			}
+			try {
+				currentExecuting = `function \`${name}\``;
+				await interpreter.execFn(value, []);
+				currentExecuting = 'root';
+			} catch (e) {
+				errorHandler.catch(currentExecuting, 'e');
+			}
+		}
+	}
+
+	return errorHandler.toResult();
 }
 
 async function runTests(dirname: string) {
 	const entries = await fs.readdir(dirname, { withFileTypes: true });
-	await Promise.allSettled(entries.map(async (entry) => {
+	await Promise.all(entries.map(async (entry) => {
 		const name = path.join(dirname, entry.name);
 		if (entry.isDirectory()) {
 			await runTests(name);
 		} else if (entry.isFile()) {
 			const result = await runTest(name);
 			if (result.success) {
-				console.log(`* test ${name}: ${chalk.green('success')}`);
+				console.log(`${chalk.green('✔')} ${name}`);
 			} else {
-				console.log(`* test ${name}: ${chalk.magenta('failure')}`);
+				console.log(`${chalk.red('✘')} ${name}`);
 				for (const error of result.errors) {
-					console.log(chalk.magenta('  ' + error.message + ': \n    ' + error.cause.toString()));
+					if (error.cause != null) {
+						console.log(chalk.red(`  • ${error.message}:\n    ${error.cause}`));
+					} else {
+						console.log(chalk.red(`  • ${error.message}`))
+					}
 				}
 			}
 		}
